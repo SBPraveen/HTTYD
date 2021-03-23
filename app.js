5ec41936dc553cb03e7f6d9bd295198dce90b4b5
PraveenLauda <46424667+SBPraveen@users.noreply.github.com>
2021-03-24 00:21:21 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+let img = document.querySelectorAll('.img');
+for(let card of img){
+    card.addEventListener('mousemove',(e)=>{
+        let xAxis = (window.innerWidth/2 - e.clientX)/10;
+        let yAxis = (window.innerHeight/2 - e.clientY)/10;
+        console.log(xAxis,yAxis);
+        card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+        
+    });
+    card.addEventListener("mouseout", (e)=>{
+        let xAxis = 0;
+        let yAxis = 0;
+        card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+    });
+    
+}
+
+
